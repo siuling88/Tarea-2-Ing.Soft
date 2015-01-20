6287cd67ffa077bb779bd832acaae12132240b8c
t0ny00 <tonyaleja@gmail.com>
2015-01-20 18:31:46 -0430

    Implementacion completada

--- Tarea-2-Ing.Soft/core/funcion.py
+++ Tarea-2-Ing.Soft/core/funcion.py
@@ -41,71 +41,84 @@ class Tarifa:
         fraccion_hora = tiempototal.seconds%3600
         fraccion_hora = int(fraccion_hora/60)
         horas_aparcado = tiempototal.seconds//3600
-        #if fraccion_hora != 0 : horas_aparcado += 1
+        if fraccion_hora == 0 : horas_aparcado += 1
         horas_aparcado += (tiempototal.days*24)
         
         for i in range(1,horas_aparcado+1):
-            if (f_en_comp + datetime.timedelta(hours=1)).hour == (6 or 18) :
+            if (f_en_comp + datetime.timedelta(hours=1)).hour == 6 :
+                f_en_comp += datetime.timedelta(hours=1)
+                totalPagar += self.tarif_Mayor()
+            elif (f_en_comp + datetime.timedelta(hours=1)).hour == 18 :
                 f_en_comp += datetime.timedelta(hours=1)
                 totalPagar += self.tarif_Mayor()
             else :
                 f_en_comp += datetime.timedelta(hours=1)
-                if f_en_comp.hour in range(7,17):
+                if f_en_comp.hour in range(7,18):
                     totalPagar += self.tarif_d 
-                elif f_en_comp.hour in range(19,24) or range(0,6) :
-                    totalPagar += self.tarif_n                            
+                elif f_en_comp.hour in range(0,6) :
+                    totalPagar += self.tarif_n  
+                    print("WTF " + str(f_en_comp.hour))
+                elif f_en_comp.hour in range(19,24) :
+                    totalPagar += self.tarif_n  
+                    print("WTF " + str(f_en_comp.hour))                           
             print(totalPagar)
             print(f_en_comp)
             print("horas " + str(i))
+            
+            
         if fraccion_hora != 0 :
             if f_en_comp.hour != (f_en_comp + datetime.timedelta(seconds = fraccion_hora*60) ).hour :
                 f_en_comp += datetime.timedelta(seconds = fraccion_hora*60)
-                if (f_en_comp.hour == (18 or 6) ) :
+                if (f_en_comp.hour == 18 ) and f_en_comp.minute != 0 :
                     totalPagar += self.tarif_Mayor()
-                elif f_en_comp.hour in range(7,18):
+                elif (f_en_comp.hour == 6 ) and f_en_comp.minute == 0 :
+                    totalPagar += self.tarif_Mayor()
+                elif f_en_comp.hour in range(6,19):
                     totalPagar += self.tarif_d 
-                elif f_en_comp.hour in range(19,24) or range(0,6) :
+                elif f_en_comp.hour in range(18,24) :
                     totalPagar += self.tarif_n 
+                elif f_en_comp.hour in range(0,6) :
+                    totalPagar += self.tarif_n
             else :
-                if f_en_comp.hour in range(6,17):
+                print("jkhkhjg " + str(totalPagar))
+                if f_en_comp.hour in range(6,18):
                     totalPagar += self.tarif_d 
-                elif f_en_comp.hour in range(19,24) or range(0,6) :
-                    totalPagar += self.tarif_n 
+                elif f_en_comp.hour in range(18,24) :
+                    print("kjkdsjhfkdf")
+                    totalPagar += self.tarif_n
+                elif f_en_comp.hour in range(0,6) :
+                    totalPagar += self.tarif_n
                     
         print(totalPagar)
         print(tiempototal)
         return totalPagar
         
 ''' M A I N '''
-"""    
-t_d = input("Introduce la tarifa diurna")    
    
-t_n = input("Introduce la tarifa nocturna")
+t_d = input("Introduce la tarifa diurna ");    
+   
+t_n = input("Introduce la tarifa nocturna ");
 
-h_en = input("Introduce la hora de entrada HH:MM")
+h_en = input("Introduce la hora de entrada HH:MM ");
 HE,ME = [int(a) for a in h_en.split(":")]
 
-h_sal = input("Introduce la hora de salida HH:MM")
+h_sal = input("Introduce la hora de salida HH:MM ");
 HS,MS = [int(b) for b in h_sal.split(":")]
 
-f_en = input("Introduce la fechaentrada YYYY-MM-DD")
+f_en = input("Introduce la fecha entrada en el formato YYYY-MM-DD");
 YYYYE,MME,DDE = [int(c) for c in f_en.split("-")]
 
-f_sal = input("Introduce la fechasalida YYYY-MM-DD")
+f_sal = input("Introduce la fecha salida en el formato YYYY-MM-DD ");
 YYYYS,MMS,DDS = [int(d) for d in f_en.split("-")]
-"""
+
 tarif = Tarifa();
 """tarif.tarif_d = t_d       
 tarif.tarif_n = t_n"""
 tarif.tarif_d = 1
 tarif.tarif_n = 5
 """totalPagar = tarif.calcularTotal(YYYYE,MME,DDE,HE,ME,YYYYS,MMS,DDS,HS,MS)"""
-totalPagar = tarif.calcularTotal(1993,4,26,13,40,1993,4,26,18,0)
+totalPagar = tarif.calcularTotal(1993,4,26,3,40,1993,4,42,6,55)
 print("El total a pagar es: "+ str(totalPagar))
 
 
-1440 = 1
-1540 = 2
-1640 = 3
-1740 = 4
-18 = 5
+
