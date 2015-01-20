343c50d2e10b325eee1c3849934ddc2d48fe9ffd
t0ny00 <tonyaleja@gmail.com>
2015-01-20 10:23:59 -0430

    uidhaee

--- Tarea-2-Ing.Soft/core/funcion.py
+++ Tarea-2-Ing.Soft/core/funcion.py
@@ -21,17 +21,20 @@ class Tarifa:
         f_sal_comp = datetime.datetime.combine(f_sal, h_sal)
         if f_sal_comp > f_en_comp:
             f_completa_total = f_sal_comp - f_en_comp 
-        elif f_sal_comp < f_en_comp:
+        elif f_sal_comp <= f_en_comp:
             print("Fechas invalidas")
-        else:
-            print("Fechas invalidas")
-        
-        tiempototal = f_en_comp - f_sal_comp
-        
+            
+        tiempototal = f_sal_comp - f_en_comp
+        if tiempototal > datetime.timedelta(3):
+            print("Excedidio el tiempo")
+        fraccion_hora = tiempototal.seconds%3600 
+        horas_aparcado = tiempototal.seconds//3600
+        if fraccion_hora != 0 : horas_aparcado += 1
+        print(horas_aparcado)
         return tiempototal
         
 ''' M A I N '''
-    
+"""    
 t_d = input("Introduce la tarifa diurna")    
    
 t_n = input("Introduce la tarifa nocturna")
@@ -40,16 +43,19 @@ h_en = input("Introduce la hora de entrada HH:MM")
 HE,ME = [int(a) for a in h_en.split(":")]
 
 h_sal = input("Introduce la hora de salida HH:MM")
-HS,MS = [int(b) for b in h_en.split(":")]
+HS,MS = [int(b) for b in h_sal.split(":")]
 
 f_en = input("Introduce la fechaentrada YYYY-MM-DD")
 YYYYE,MME,DDE = [int(c) for c in f_en.split("-")]
 
 f_sal = input("Introduce la fechasalida YYYY-MM-DD")
 YYYYS,MMS,DDS = [int(d) for d in f_en.split("-")]
-
+"""
 tarif = Tarifa();
-tarif.tarif_d = t_d
-tarif.tarif_n = t_n
-totalPagar = tarif.calcularTotal(YYYYE,MME,DDE,HE,ME,YYYYS,MMS,DDS,HS,MS)
+"""tarif.tarif_d = t_d
+tarif.tarif_n = t_n"""
+tarif.tarif_d = 1
+tarif.tarif_n = 2
+"""totalPagar = tarif.calcularTotal(YYYYE,MME,DDE,HE,ME,YYYYS,MMS,DDS,HS,MS)"""
+totalPagar = tarif.calcularTotal(1993,4,28,6,40,1993,4,28,9,41)
 print("El total a pagar es: "+ str(totalPagar))
